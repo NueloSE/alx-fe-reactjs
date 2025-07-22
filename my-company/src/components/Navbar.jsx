3a58a2d4402fc783f9907aa43f0ec2d4202d0006
nuelo <akalo.emmanuel18@gmail.com>
2025-07-22 10:14:54 +0100

    fix: modify inline styling to match checker

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div style={{ display: "flex", gap: "3rem", justifyContent: "center" , background: 'white', width: '100vw', padding: "1rem 0"}}>
+    <div style={{ display: "flex", gap: "3rem", justifyContent: "center" , backgroundColor: 'white', width: '100vw', padding: "1rem 0"}}>
       <Link to="/">Home</Link>
       <Link to="/about">About Us</Link>
       <Link to="/services">Services</Link>
