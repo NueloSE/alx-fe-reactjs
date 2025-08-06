3f4fdcb403bdda9048c0d9376c2839bec9c7a4de
nuelo <akalo.emmanuel18@gmail.com>
2025-08-06 15:09:09 +0100

    feat: create delete recipe component

---
+++ recipe-sharing-app/src/components/DeleteRecipe.jsx
@@ -0,0 +1,24 @@
+import useRecipeStore from "./recipeStore";
+import { useNavigate } from "react-router-dom";
+
+
+function DeleteRecipe ({recipeId}) {
+
+    const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+    const navigate = useNavigate();
+
+    function deleteFn(id) {
+      deleteRecipe(id);
+      navigate("/list");
+    }
+
+    return (
+      <button className="delete-btn" onClick={() => deleteFn(recipeId)}>
+        Delete Recipe
+      </button>
+    );
+
+
+}
+
+export default DeleteRecipe;
