2271ff7ed6b65eaddce4d6b1e31dd19d3ad32650
nuelo <akalo.emmanuel18@gmail.com>
2025-08-07 23:04:46 +0100

    chores: rename files to meet checker requirement

--- recipe-sharing-app/src/components/HomePage.jsx
+++ recipe-sharing-app/src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import FavouritesList from "./FavouritesList";
+import FavouritesList from "./FavoritesList";
 import RecommendationsList from "./RecommendationsList";
 
 function HomePage() {
