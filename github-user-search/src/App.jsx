b7c94502e838465b50cc752f125574bd90c3e684
nuelo <akalo.emmanuel18@gmail.com>
2025-08-11 13:37:07 +0100

    feat: create a not found page

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Search from "./components/Search";
 import AdvancedSearch from "./components/AdvancedSearch";
+import NotFound from "./components/NotFound";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />}/>
         <Route path="/advanced" element={<AdvancedSearch/>} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     
     </BrowserRouter>
